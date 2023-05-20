19081dc097ecfeb9284d1e74337d6c2c1f9c97c0
Frank <frank051284@gmail.com>
2023-05-20 22:53:03 +0800

    add db function

--- models/_db.js
+++ models/_db.js
@@ -1,15 +1,35 @@
+// const Sequelize = require('sequelize');
+// const db = new Sequelize(process.env.DATABASE_URL, {
+//     dialect: 'postgres',
+//     dialectOptions:{
+//         ssl:true,
+//     },
+//     pool:{
+//         min: 0,
+//         max:5,
+//         acquire:30000,
+//         idle: 10000,
+//     }
+// });
+
+// module.exports = db;
+
+
+
 const Sequelize = require('sequelize');
 const db = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres',
-    dialectOptions:{
-        ssl:true,
-    },
-    pool:{
-        min: 0,
-        max:5,
-        acquire:30000,
-        idle: 10000,
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      rejectUnauthorized: false
     }
+  },
+  pool: {
+    min: 0,
+    max: 5,
+    acquire: 30000,
+    idle: 10000,
+  }
 });
 
 module.exports = db;
